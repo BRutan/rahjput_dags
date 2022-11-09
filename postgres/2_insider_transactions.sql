e7d10973a086f48c8f17697ec2b64957b8aeefc9
brutan <rutan.benjamin@gmail.com>
2022-11-09 01:13:42 -0500

    Fixed lack of schema.

--- postgres/2_insider_transactions.sql
+++ postgres/2_insider_transactions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS rahjput_data.insider_transactions
 (
-    company_id SERIAL REFERENCES tickers_to_track(company_id),
+    company_id SERIAL REFERENCES rahjput_data.tickers_to_track(company_id),
     name VARCHAR,
     share INT,
     change INT,
